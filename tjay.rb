0623ee0ff5d6fa6fb6297fd22adddbffad388935
saradajani <37843528+saradajani@users.noreply.github.com>
2020-01-21 14:40:11 -0800

    Add files via upload

---
+++ tjay.rb
@@ -0,0 +1,21 @@
+use_bpm 97
+use_synth :piano
+
+play :g3, sustain: 5
+play :e4, sustain: 5
+sleep 1.5
+play :c5
+sleep 0.25
+play :d5
+sleep 1.5
+play :d5
+play :f3, sustain: 5
+sleep 1
+play :b4
+sleep 0.5
+play :b4
+sleep 0.3
+play :a4
+sleep 0.3
+play :b4, sustain:5
+sleep 0.25

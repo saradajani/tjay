990b4f69e89b773f5a85a31e2ed5baf74ca37e6d
saradajani <37843528+saradajani@users.noreply.github.com>
2020-01-23 14:38:40 -0800

    Add files via upload

---
+++ liltjay.rb
@@ -0,0 +1,98 @@
+use_bpm 92
+use_synth :beep
+play :g3, sustain: 5
+play :e4, sustain: 5
+play :d4
+sleep 2.5
+play :c5
+sleep 0.25
+play :d5
+sleep 1.5
+play :d4
+play :a3
+play :f3, sustain: 5
+sleep 1.25
+play :b4
+sleep 0.75
+play :a4
+sleep 0.25
+play :b4
+sleep 1.5
+play :d4
+play :b3
+play :f3, sustain:5
+sleep 2
+play :c4
+play :b3
+play :g3, sustain:5
+sleep 0.5
+play :d4
+play :a3
+play :f3
+sleep 1
+play :b4
+sleep 0.5
+play :a4
+sleep 0.25
+play :a4
+sleep 0.75
+play :g3, sustain: 5
+play :e4, sustain: 5
+play :d4
+sleep 2.5
+play :d5
+sleep 1
+play :d4
+play :a3
+play :f3, sustain: 5
+sleep 0.75
+play :b4
+sleep 2.5
+play :a4
+sleep 0.25
+play :c4
+sleep 0.25
+play :b4
+sleep 1
+play :d4
+play :b3
+play :f3, sustain:5
+sleep 2
+play :c4
+play :b3
+play :g3, sustain:5
+sleep 2
+play :g3, sustain: 5
+play :e4, sustain: 5
+play :d4
+sleep 0.5
+play :d4
+sleep 0.25
+play :d4
+sleep 0.25
+play :d4
+sleep 0.5
+play :b4
+sleep 0.5
+play :g4
+sleep 0.5
+play :g4
+sleep 0.25
+play :a4
+sleep 0.25
+play :b4
+sleep 0.5
+play :a4
+sleep 0.5
+play :b4
+sleep 0.25
+play :b4
+sleep 0.25
+play :b4
+sleep 0.25
+play :b4
+sleep 0.25
+play :b4
+sleep 0.25
+play :b4
+sleep 0.25
